080a2e4ab127ae96f80a85027e8a761a7f1c537d
JuliaSerrano <jserranoarrese@gmail.com>
2024-10-29 19:12:39 +0100

    complete exercises 1-5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(nums) {
+    let numsToRemove = [];
+    for (let i = 1; i < arguments.length; i++){
+        numsToRemove.push(arguments[i]);
+    }
+
+    return nums.filter((num) => !numsToRemove.includes(num));
+
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    // check types
+    if ((num1 < 0 || num2 < 0) || (!Number.isInteger(num1) || !Number.isInteger(num2))){
+        return 'ERROR';
+    }
+    let lowNum, highNum;
+    highNum = num1 > num2 ? num1 : num2;
+    lowNum = num1 > num2 ? num2 : num1;
 
+    let sum = 0;
+    for (lowNum; lowNum <= highNum; lowNum++){
+        sum += lowNum;
+    }
+    return sum;
+    
 };
 
 // Do not edit below this line
