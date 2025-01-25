5b61ea5545f55f5a5e1bfae6f9603ab3ae0774a5
JuliaSerrano <jserranoarrese@gmail.com>
2025-01-25 20:03:29 +0100

    add 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const clean_string = string
+        .replace(/[^0-9a-z-A-Z]/g, "").replace(/ +/, " ")
+        .toLowerCase();
+
+    let p1 = 0;
+    let p2 = clean_string.length - 1;
+
+    while (p2 > p1){
+        if (clean_string[p1] !== clean_string[p2]){
+            return false
+        }
+        p1++;
+        p2--; 
+    }
+    return true;
 
 };
 
